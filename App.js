9f2d4e63ab5c536e83579f44073b2a1fa9376665
Matheus <sujeitoprogramador@gmail.com>
2019-04-14 22:07:59 -0300

    atualizando estilos

--- App.js
+++ App.js
@@ -23,45 +23,45 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-      <View style={{width:'100%', height:55, backgroundColor: '#DDD', alignItems: 'center', justifyContent: 'center'}}>
+      <View style={styles.header}>
         <Text style={{fontSize: 25, color: '#000'}}>Instagram</Text>
       </View>
+
       <ScrollView>
       {this.state.posts.map((post)=>{
         return(
-          <View key={post.key}>
+        <View key={post.key}>
+
           <ShimmerPlaceHolder
-          style={{height: 40, margin: 10, borderRadius: 10 }}
+          style={{height: 35, margin: 10, borderRadius: 10 }}
           autoRun={true}
-          visible={this.state.visible}
-          >
-  
-        <View style={{width: '100%' ,height: 55, flexDirection:'row', alignItems: 'center'}}>
-          
-          <Image source={{uri:post.image}} style={{width: 40, height:40, borderRadius: 25, marginLeft:10}}/>
-          
-          <Text style={{color: '#000', fontWeight:'bold', marginLeft: 10}}>
-            {post.username}
-          </Text>
-        </View>
+          visible={this.state.visible}>
+
+            <View style={styles.postHeader}>
+              <Image source={{uri:post.image}} style={styles.imgPerfil}/>
+              <Text style={styles.username}>
+                {post.username}
+              </Text>
+            </View>
+
         </ShimmerPlaceHolder>
   
         <ShimmerPlaceHolder
           style={{width: '100%' ,height: 300 }}
           autoRun={true}
-          visible={this.state.visible}
-        >
+          visible={this.state.visible}>
   
-        <View style={{backgroundColor:'#DDD', width: '100%' ,height: 300}}>
-         <Image source={{uri:post.image}} style={{height:300}}/>
-        </View>
+          <View style={styles.postImg}>
+           <Image source={{uri:post.image}} style={styles.image}/>
+          </View>
+
         </ShimmerPlaceHolder>
-        </View>
+        
+       </View>
         );
       })}
       </ScrollView>
 
-      
       </View>
     );
   }
@@ -71,4 +71,36 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  header:{
+    width:'100%', 
+    height:55, 
+    backgroundColor: '#DDD', 
+    alignItems: 'center', 
+    justifyContent: 'center'
+  },
+  imgPerfil:{
+    width: 40, 
+    height:40, 
+    borderRadius: 25, 
+    marginLeft:10
+  },
+  postImg:{
+    backgroundColor:'#DDD', 
+    width: '100%',
+    height: 300
+  },
+  username:{
+    color: '#000', 
+    fontWeight:'bold', 
+    marginLeft: 10
+  },
+  postHeader:{
+    width: '100%' ,
+    height: 55, 
+    flexDirection:'row', 
+    alignItems: 'center'
+  },
+  image:{
+    height:300
+  }
 });
